922e2258551c5caefb472723e9bbb77b776d6be9
sunnyfixer3941 <sunnyfixer3941@gmail.com>
2019-06-24 13:00:01 +0530

    Updates

---
+++ precache-manifest.621c4e00cf69e8afa0d91d5487610e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43cea23e20b186c3833834e480713d42",
+    "url": "/actest/index.html"
+  },
+  {
+    "revision": "54bbdecd19046d409e7f",
+    "url": "/actest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/actest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "54bbdecd19046d409e7f",
+    "url": "/actest/static/js/main.6657f55b.chunk.js"
+  },
+  {
+    "revision": "e44665ab51a50f0ba588",
+    "url": "/actest/static/js/runtime~main.1fcaaac6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/actest/static/media/logo.5d5d9eef.svg"
+  }
+]);
